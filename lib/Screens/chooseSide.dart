ba5fff261fc48d2ac8186506535602d8c1ee28d3
Mariobahaa <mariobahaa59s@gmail.com>
2019-09-14 13:24:28 +0200

    added home sceen, assets, fonts and audioplayers

---
+++ lib/Screens/chooseSide.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ChooseSide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Screens/homeScreen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      height: 100.0,
+                      width: 100.0,
+                      child: Image.asset('assets/Tic-Tac-Toe-Game.ico')),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'Tic Tac Toe',
+                    style: TextStyle(
+                        color: Color(0xFF27AAE1),
+                        fontSize: 42.0,
+                        fontFamily: 'PermanentMarker'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              onPressed: () {
+                Navigator.pushNamed(context, '/side');
+              },
+              color: Color(0xFF27AAE1),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.person,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 18.0,
+                    ),
+                    Text(
+                      '1 Player',
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontFamily: 'PermanentMarker',
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/game');
+                },
+                color: Colors.red,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      SizedBox(
+                        width: 18.0,
+                      ),
+                      Text(
+                        '2 Players',
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            fontFamily: 'PermanentMarker',
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: SizedBox(),
+          ),
+        ],
+      ),
+    );
+  }
+}
